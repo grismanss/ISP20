92b51c1db14a78393fbe7b12ae13ce460dfdbadd
s.s.grisman@mail.ru <s.s.grisman@mail.ru>
2021-04-13 11:39:01 +0700

    13/04/2021

---
+++ 13.04.2021/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Пользовател вводит два числа, вывести наибольшее */
+
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
